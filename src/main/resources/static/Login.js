e3bd6fd5a31e209ec1d258e5b84c6203023b3b25
Erica <138564331+EricaHowell@users.noreply.github.com>
2023-07-13 23:27:43 +1000

    added cosmetic changes and signin/login

--- src/main/resources/static/Login.js
+++ src/main/resources/static/Login.js
@@ -3,3 +3,8 @@ function triggerLogin(){
     let login = new bootstrap.Modal(loginModal, {})
     login.show();
 }
+function triggerSignup(){
+    const signupModal = document.getElementById('signup-modal')
+    let signup = new bootstrap.Modal(signupModal, {})
+    signup.show();
+}
