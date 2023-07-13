9536f0a8c8724c7385cdafdcb98df18bf77e5c62
Erica <138564331+EricaHowell@users.noreply.github.com>
2023-07-14 08:13:23 +1000

    last update

--- src/main/resources/static/Login.js
+++ src/main/resources/static/Login.js
@@ -28,13 +28,16 @@ function loginCheck(){
 
     const loginModal = document.getElementById('login-modal')
     let login = new bootstrap.Modal(loginModal, {})
-    login.hide()
+    login.show()
 }
+
+
 function triggerSignup(){
     const signupModal = document.getElementById('signup-modal')
     let signup = new bootstrap.Modal(signupModal, {})
     signup.show();
 }
+
 
 function newUserSignup(){
     var inputUsername = document.getElementById("Signup-username").value
@@ -45,3 +48,52 @@ function newUserSignup(){
     fetch("/users", {method:"POST",
         headers:{'Accept': 'application/json', 'Content-Type':'application/json'}})
 }
+
+//
+//
+// function postEvent(){
+//
+//     var data = {
+//         "title": document.getElementById('Event-title').value,
+//         "description": document.getElementById('description-text').value,
+//         "startTime": Math.floor(new Date(document.getElementById('start-datetime').value+":00.000Z").getTime()/1000),
+//         "endTime": Math.floor(new Date(document.getElementById('end-datetime').value+":00.000Z").getTime()/1000),
+//         "userid": 1
+//     }
+//
+//
+//     fetch('/events', {method: "POST", headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         }, body: JSON.stringify(data)})
+//         .then(function (resp) {
+//             console.log(resp)
+//
+//         })
+//         .catch(function (error) {
+//             console.error(error);
+//         });
+//
+//     // getEventsfromDB()
+//     const eventcreator = document.getElementById('event-creator')
+//     let myModal = new bootstrap.Modal(eventcreator, {})
+//
+//     document.getElementById('Event-title').value = ""
+//     document.getElementById('description-text').value = ""
+//     myModal.hide();
+//
+//     calender.refetchEvents()
+// }
+//
+//
+//
+//
+//
+//
+// function logout(){
+//     sessionStorage.setItem('userid', null)
+// }
+//
+// function editUser(){
+//
+//}
